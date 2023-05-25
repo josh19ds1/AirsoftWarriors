9ca4a6a26203f3074de4fbe767fad120e814fa2d
josh19ds1 <josephnefgon@gmail.com>
2023-05-24 21:30:47 -0500

    ""

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import { Suspense } from "react";
+import { fetchData } from "./componentes/Service/fetchData"
 
 import './App.css';
 import PG_Head from './componentes/head/PG_Head';
 import PaginaPrincipal from './componentes/PaginaPrincipal';
-
 import { Diseño } from './componentes/head/diseño';
 
-
+const apiData = fetchData("https://nodejs-restapi-airsoft-warrior-production-8daf.up.railway.app/api/products");
 
 function App() {
+  const data = apiData.read();
+
   return (
 
 
     <main>
       <PG_Head />
-     
+
       <h1>Probando informacion generada</h1>
+
+      <Suspense fallback={<div>Loading....</div>}>
+        <ul className="card" style={{ color: "#fff" }}>
+          {data?.map((product) => (
+            <li key={product.id}>{product.name}
+                    <img src={product.url} alt="product airsoft"/>
+                    {console.log(product.url)}
+            
+            </li>
+    
+          ))}
+        </ul>
+      </Suspense>
       <PaginaPrincipal />
 
       <Diseño />
---
+++ src/componentes/Service/fetchData.js
@@ -0,0 +1,51 @@
+
+
+
+const getSuspender = (promise) =>{
+    let status = "pending";
+    let response;
+
+
+        //En caso de que este suspendido
+    const suspender = promise.then(
+
+        (res) => {
+            status = "success";
+            response = res;
+
+        },
+        (err) => {
+
+            status = "error";
+            response = err;
+
+        }
+    )
+
+    //funcion de lectura
+    const read = ()=>{
+
+        switch(status){
+            case "pending":
+                throw suspender;
+                
+            case "error":
+                throw response;
+                
+            default:
+               return response;
+        }
+
+    };
+    return {read};
+};
+
+
+
+export function fetchData(url) {
+
+    const promise = fetch(url)
+        .then((response) => response.json())
+        .then((data) => data)
+    return getSuspender(promise)
+}
