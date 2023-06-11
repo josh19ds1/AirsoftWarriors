6e6acdcd5d1d3d6e873c5935837401f13e802a7b
iCreative2019 <alfonsob2018@gmail.com>
2023-06-11 01:17:30 -0500

    Poniendo el CSS
    Falta arreglar el Swiper y poner las imagenes

--- src/componentes/Paginas-Proyecto/Evento.jsx
+++ src/componentes/Paginas-Proyecto/Evento.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
+import '../../estilos/events.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
 
 const Evento = () => {
   return (
 
-    <><section class="home" id="home">
+    <><><section class="home" id="home">
       <div class="content">
         <h3>Airsoft <span>Event</span></h3>
       </div>
 
       <div class="swiper mySwiper home-slider">
         <div class="swiper-wrapper">
-          <div class="swiper-slide"><img src="images/image1.jpg" alt=""/></div>
-          <div class="swiper-slide"><img src="images/image2.jpg" alt=""/></div>
-          <div class="swiper-slide"><img src="images/image3.jpg" alt=""/></div>
-          <div class="swiper-slide"><img src="images/image4.jpg" alt=""/></div>
-          <div class="swiper-slide"><img src="images/image5.jpg" alt=""/></div>
-          <div class="swiper-slide"><img src="images/image6.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image1.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="images/image2.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="images/image3.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="images/image4.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="images/image5.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="images/image6.jpg" alt="" /></div>
         </div>
       </div>
 
@@ -62,13 +68,12 @@ const Evento = () => {
         <h1 class="heading">About <span>Us</span></h1>
         <div class="row">
           <div class="image">
-            <img src="images/aboutus.jpg" alt=""/>
-            </div>
+            <img src="images/aboutus.jpg" alt="" />
+          </div>
           <div class="content">
             <h3>Airsoft Warrior Team</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo eveniet quae corrupti consequatur quidem. Asperiores a praesentium illo nemo, magnam illum ea sed voluptate!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!</p>
-            <a href="#" class="btn">Contact us</a>
           </div>
         </div>
       </section><section class="gallery" id="gallery">
@@ -85,56 +90,121 @@ const Evento = () => {
           </div>
 
           <div class="box">
-            <img src="images/gall2.jpeg" alt=""/>
-              <h3 class="title">Zona Urbana</h3>
-              <div class="icons">
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-share"></a>
-                <a href="#" class="fas fa-eye"></a>
-              </div>
+            <img src="images/gall2.jpeg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
             </div>
+          </div>
 
           <div class="box">
-            <img src="images/gall3.jpg" alt=""/>
-              <h3 class="title">Zona Urbana</h3>
-              <div class="icons">
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-share"></a>
-                <a href="#" class="fas fa-eye"></a>
-              </div>
+            <img src="images/gall3.jpg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
             </div>
+          </div>
 
           <div class="box">
-            <img src="images/gall4.jpg" alt=""/>
-              <h3 class="title">Zona Urbana</h3>
-              <div class="icons">
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-share"></a>
-                <a href="#" class="fas fa-eye"></a>
-              </div>
+            <img src="images/gall4.jpg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
             </div>
+          </div>
 
           <div class="box">
-            <img src="images/gall4.jpg" alt=""/>
-              <h3 class="title">Zona Urbana</h3>
-              <div class="icons">
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-share"></a>
-                <a href="#" class="fas fa-eye"></a>
-              </div>
+            <img src="images/gall4.jpg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
             </div>
+          </div>
 
           <div class="box">
-            <img src="images/gall4.jpg" alt=""/>
-              <h3 class="title">Zona Urbana</h3>
-              <div class="icons">
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-share"></a>
-                <a href="#" class="fas fa-eye"></a>
-              </div>
+            <img src="images/gall4.jpg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
             </div>
+          </div>
         </div>
       </section></>
+      
+      <section class="price" id="price">
+
+        <h1 class="heading"> Nuestros <span>Precios</span></h1>
+        <div class="box-container">
+          <div class="box">
+            <h3 class="title">Package 1</h3>
+            <h3 class="amount">$250.99</h3>
+            <ul>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+
+            </ul>
+            <a href="" class="btn">Check Out</a>
+          </div>
+
+          <div class="box">
+            <h3 class="title">Package 2</h3>
+            <h3 class="amount">$250.99</h3>
+            <ul>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+
+            </ul>
+            <a href="" class="btn">Check Out</a>
+          </div>
+
+          <div class="box">
+            <h3 class="title">Package 3</h3>
+            <h3 class="amount">$550.99</h3>
+            <ul>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+
+            </ul>
+            <a href="" class="btn">Check Out</a>
+          </div>
+
+          <div class="box">
+            <h3 class="title">Package 4</h3>
+            <h3 class="amount">$850.99</h3>
+            <ul>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+              <li><i class="fas fa-check"></i>Full services</li>
+
+            </ul>
+            <a href="" class="btn">Check Out</a>
+          </div>
+        </div>
+
+      </section></>
+
+      
   )
 }
 
