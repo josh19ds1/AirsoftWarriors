5649fca67e6fdf8607fd3abc808870b988b4520d
iCreative2019 <alfonsob2018@gmail.com>
2023-06-10 19:02:30 -0500

    Update Evento
    Poniendo el html (aun no ta completo)

--- src/componentes/Paginas-Proyecto/Evento.jsx
+++ src/componentes/Paginas-Proyecto/Evento.jsx
@@ -2,7 +2,139 @@ import React from 'react'
 
 const Evento = () => {
   return (
-    <div>Evento</div>
+
+    <><section class="home" id="home">
+      <div class="content">
+        <h3>Airsoft <span>Event</span></h3>
+      </div>
+
+      <div class="swiper mySwiper home-slider">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"><img src="images/image1.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image2.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image3.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image4.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image5.jpg" alt=""/></div>
+          <div class="swiper-slide"><img src="images/image6.jpg" alt=""/></div>
+        </div>
+      </div>
+
+    </section><section class="service" id="service">
+        <h1 class="heading">Nuestros <span>Servicios</span></h1>
+        <div class="box-container">
+          <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+
+          <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+
+          <div class="box">
+            <i class="fas fa-music"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+
+          <div class="box">
+            <i class="fas fa-utensils"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+
+          <div class="box">
+            <i class="fas fa-photo-video"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+
+          <div class="box">
+            <i class="fas fa-birthday-cake"></i>
+            <h3>Captura la Bandera</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, debitis.</p>
+          </div>
+        </div>
+      </section><section class="about" id="about">
+        <h1 class="heading">About <span>Us</span></h1>
+        <div class="row">
+          <div class="image">
+            <img src="images/aboutus.jpg" alt=""/>
+            </div>
+          <div class="content">
+            <h3>Airsoft Warrior Team</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo eveniet quae corrupti consequatur quidem. Asperiores a praesentium illo nemo, magnam illum ea sed voluptate!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse!</p>
+            <a href="#" class="btn">Contact us</a>
+          </div>
+        </div>
+      </section><section class="gallery" id="gallery">
+        <h1 class="heading">Play <span>Places</span></h1>
+        <div class="box-container">
+          <div class="box">
+            <img src="images/gall1.jpg" alt="" />
+            <h3 class="title">Zona Urbana</h3>
+            <div class="icons">
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-eye"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/gall2.jpeg" alt=""/>
+              <h3 class="title">Zona Urbana</h3>
+              <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+              </div>
+            </div>
+
+          <div class="box">
+            <img src="images/gall3.jpg" alt=""/>
+              <h3 class="title">Zona Urbana</h3>
+              <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+              </div>
+            </div>
+
+          <div class="box">
+            <img src="images/gall4.jpg" alt=""/>
+              <h3 class="title">Zona Urbana</h3>
+              <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+              </div>
+            </div>
+
+          <div class="box">
+            <img src="images/gall4.jpg" alt=""/>
+              <h3 class="title">Zona Urbana</h3>
+              <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+              </div>
+            </div>
+
+          <div class="box">
+            <img src="images/gall4.jpg" alt=""/>
+              <h3 class="title">Zona Urbana</h3>
+              <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+              </div>
+            </div>
+        </div>
+      </section></>
   )
 }
 
