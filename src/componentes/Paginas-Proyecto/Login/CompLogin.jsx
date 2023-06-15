81f3bf17ffec292c090d2832db0b1d12386d6ba8
josh19ds1 <josephnefgon@gmail.com>
2023-06-15 09:59:29 -0500

    "loogin"

--- src/componentes/Paginas-Proyecto/Login/CompLogin.jsx
+++ src/componentes/Paginas-Proyecto/Login/CompLogin.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
 
 
+
 const CompLogin = () => {
 
   const handleLoginFormSubmit = (e) => {
@@ -37,8 +38,17 @@ const CompLogin = () => {
   return <div className="login-structure">
         <h1 className="login-title">Login</h1>
         <Button className="btn-login-google"  href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Fnodejs-restapi-airsoft-warrior-production-8daf.up.railway.app%2Fapi%2Fauth%2Fgoogle&client_id=432056390247-at62ppadiuih66mndsb42mufjmflafac.apps.googleusercontent.com&access_type=offline&response_type=code&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email"  
-                variant="contained"
+                variant="container"
                 startIcon={<i className="fab fa-google left"></i>}
+                sx={{  
+                  textAlign: 'center',
+                  fontSize: '20px',
+                  borderRadius: '3px',
+                  padding: '15px',
+                  marginLeft: '36.5%',
+                  color:'#ffff',
+                  background:'#E67D06'
+                }}
         >Iniciar sesión con Google </Button>
 
         <hr className="line"></hr>
