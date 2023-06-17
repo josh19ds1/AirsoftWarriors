591dbcc2928bc27daea420b9187fcd2edd691520
josh19ds1 <josephnefgon@gmail.com>
2023-06-17 16:34:38 -0500

    "version 5.1 loged"

--- src/componentes/Paginas-Proyecto/Login/UserExist.jsx
+++ src/componentes/Paginas-Proyecto/Login/UserExist.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Dominio,ApiUserExist } from '../../Tools/var'
-import fetchData from '../../Service/feetchApi'
+import React, { useEffect, useState } from 'react';
+import { Dominio, ApiUserExist } from '../../Tools/var';
+import fetchData from '../../Service/feetchApi';
 
-const apiUrl=`${Dominio}/${ApiUserExist}`
-
+const apiUrl = `${Dominio}/${ApiUserExist}`;
 
 const UserExist = () => {
-const [data,setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchDataFromApi = async () => {
-            const responseData = await fetchData(apiUrl);
-            setData(responseData);
-        };
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      const responseData = await fetchData(apiUrl);
+      setData(responseData);
+    };
 
-        fetchDataFromApi();
-    }, []);
-console.log(data)
-if (data && data.loggedIn === true) {
-    return <h1>Existe</h1>;
+    fetchDataFromApi();
+  }, []);
+
+  console.log(data);
+
+  if (data && typeof data.loggedIn === 'boolean') {
+    if (data.loggedIn) {
+      return <h1>Existe</h1>;
+    } else {
+      return <h1>No existe</h1>;
+    }
   } else {
-    return <h1>No existe</h1>;
+    return null; // Otra opción es mostrar un mensaje de carga mientras se obtiene la respuesta
   }
-};  
+};
 
-export default UserExist
+export default UserExist;
