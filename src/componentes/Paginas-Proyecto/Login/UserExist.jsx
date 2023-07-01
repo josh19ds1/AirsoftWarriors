b73b866169472d16165e8013cf75a9f60e4378e1
josh19ds1 <josephnefgon@gmail.com>
2023-07-01 15:17:06 -0500

    "eliminando console del userExist"

--- src/componentes/Paginas-Proyecto/Login/UserExist.jsx
+++ src/componentes/Paginas-Proyecto/Login/UserExist.jsx
@@ -9,7 +9,7 @@ const apiUrl = `${Dominio}/${ApiUserExist}`;
 const UserExist = () => {
   const [data, setData] = useState(null);
   const dispatch = useDispatch();
-  console.log("url del servidor que estoy usando:"+apiUrl);
+
 
   useEffect(() => {
     const fetchDataFromApi = async () => {
@@ -42,10 +42,10 @@ const UserExist = () => {
     fetchDataFromApi();
   }, []);
 
-  console.log("data:"+data);
+
 
   if (data && typeof data.isSuccess === 'boolean') {
-    console.log("variable que busco=" + data.isSuccess);
+   
    
     if (data.isSuccess) {
       dispatch(setUserExist(true));
