3bd41c17bead9cb1604bc6b3312a1c14df7f4a5b
josh19ds1 <josephnefgon@gmail.com>
2023-07-04 14:48:38 -0500

    "prueba 4"

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -22,13 +22,57 @@ import EmpName from './SubMenuNavBar/EmpName'
 import CarritoNav from './SubMenuNavBar/CarritoNav'
 import { useSelector } from 'react-redux'
 import '../../estilos/font.css';
-import { Dominio, ApiPerfil } from '../Tools/var'
-import CerrarSesion from '../Paginas-Proyecto/Login/CerrarSecion';
+import { Dominio, ApiPerfil,ApiLogout } from '../Tools/var'
+import { useDispatch } from "react-redux";
+import { setUserExist } from '../../Store/userLogin/userExist'
 
 
 const apiUrl = `${Dominio}/${ApiPerfil}`
+const apiUrlLs = `${Dominio}/${ApiLogout}`;
 
+const CerrarSesion = () => {
+  const [data, setData] = useState(null);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        const response = await fetch(apiUrlLs, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          redirect: "follow",
+        });
+
+        if (!response.ok) {
+          throw new Error(`Request failed with status ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        setData(responseData);
+
+        if (responseData.url) {
+          console.log("data-url=" + responseData.url);
+          window.location.href = responseData.url;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchDataFromApi();
+  }, []);
+
+  useEffect(() => {
+    if (data && typeof data.isSuccess === "boolean") {
+      dispatch(setUserExist(!data.isSuccess));
+    }
+  }, [data, dispatch]);
+
+  return null;
+};
 
 const settings = ['Perfil', 'Carrito', 'Cerrar Sesion']
 
