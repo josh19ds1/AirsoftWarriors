ea77457045a1e9253e8774db6c8ab2cdd5e643e1
josh19ds1 <josephnefgon@gmail.com>
2023-07-14 17:05:29 -0500

    borre comentarios

--- src/componentes/Paginas-Proyecto/Login/CerrarSecion.jsx
+++ src/componentes/Paginas-Proyecto/Login/CerrarSecion.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { Dominio, ApiLogout } from "../../Tools/var";
 import { setUserExist } from "../../../Store/userLogin/userExist";
 import { useDispatch } from "react-redux";
@@ -12,45 +12,37 @@ const CerrarSesion = () => {
   useEffect(() => {
     const fetchDataFromApi = async () => {
       try {
-        const response = await fetch('https://nodejs-restapi-airsoft-warrior-production-8daf.up.railway.app/api/auth/Logout', {
+        const response = await fetch(apiUrl, {
           method: 'GET',
-          credentials: 'include',
-        
-        });
-          console.log('1');
-
-
+          credentials: 'include',      
+        }); 
         if (response.ok) {
-          console.log('2');
-           console.log('response: '+response.ok);
           throw new Error(`Request failed with status ${response.status}`);
         }
 
-
-       
         const responseData = await response.json();
         setData(responseData);
-        console.log('3');
+  
         if (responseData.url) {
-          console.log("data-url=" + responseData.url);
+      
           window.location.href = responseData.url;
         }
       } catch (error) {
-        console.log('4');
+      
         console.log(error);
       }
     };
-    console.log('5');
+
     fetchDataFromApi();
   }, []);
 
   if (data && typeof data.isSuccess === 'boolean') {
-    console.log('6');
+
     if (data.isSuccess) {
-      console.log('7');
+
       dispatch(setUserExist(true));
     } else {
-      console.log('8');
+   
       dispatch(setUserExist(false))
     }
   }
