7d5f8a871aa9b91f82a3b985f69e3a4f80a0da1d
josh19ds1 <josephnefgon@gmail.com>
2023-07-16 16:00:23 -0500

    "viendo el perfil 0.1"

--- src/componentes/Paginas-Proyecto/Perfil.jsx
+++ src/componentes/Paginas-Proyecto/Perfil.jsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react'
 import "../../estilos/Perfil.css"
 import { Dominio, ApiPerfil } from '../Tools/var'
 import { Image } from '@mui/icons-material'
+import { Alert } from '@mui/material'
 
 
-const apiUrl =`${Dominio}/${ApiPerfil}`
+const apiUrl = `${Dominio}/${ApiPerfil}`
 const Perfil = () => {
   const [data, setData] = useState([]);
 
@@ -43,25 +44,27 @@ const Perfil = () => {
 
   return (
     <div>
-    <div className="perfil-body">
-      <div className="square-perfil">
-        <div className="square-photo">
-          {/* Esto es para arreglar el diseño, luego se puede eliminar */}
-          {data?.map((profile) => (
-            <div key={profile.id}>
-              <Image src={profile.img} alt="" />
-              <div className="square-text">
-                <h1>Nombre: {profile.name}</h1>
-                <h1>Apellido: {profile.lastname}</h1>
-                <h1>Email: {profile.email}</h1>
+      <div className="perfil-body">
+        <div className="square-perfil">
+          <div className="square-photo">
+            {/* Esto es para arreglar el diseño, luego se puede eliminar */}
+            {data ? (
+              <div>
+                <Image src={data.image_url.url} alt={data.image_url.id} />
+                <div className="square-text">
+                  <h1>Nombre: {data.name}</h1>
+                  <h1>Apellido: {data.lastname}</h1>
+                  <h1>Email: {data.email}</h1>
+                </div>
               </div>
-            </div>
-          ))}
+            ) : (
+              <Alert>No hay datos disponibles</Alert>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  
+
   );
 };
 
